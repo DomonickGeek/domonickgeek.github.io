8fed488ef742241f2f8ee2f6caf9a9ddb70d4158
Nathan Wallen <nathan.wallen@geeksi.tech>
2023-06-28 15:30:47 -0400

    fixed JavaScript functionality on index.html and quiz.js

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,13 +1,29 @@
+//JavaScript File used for handling the quiz feature on the home page (index.html)
+
+//enables toggling between quiz being shown and hidden. Resets the resultsDisplay elements text and color.
+//Is called by the face image
 function runQuiz(){
+    var resultText = document.getElementById("resultsDisplay");
+    resultText.textContent = "";
+    resultText.style="color:black";
     var container = document.getElementById("quizContainer");
-    if(container.style.visibility === "hidden"){
-        container.style.visibility = "visible";
+    if(container.hidden === true){
+        container.hidden = false;
     }
-    else if(container.style.visibility === "visible"){
-        container.style.visibility = "hidden";
+    else if(container.hidden === false){
+        container.hidden = true;
     }
 }
 
+//Displays a correct or incorrect prompt based off what button element the user has selected when the submit button is pressed
+//Is called by the submit button
 function checkAnswer(){
-
+    var resultText = document.getElementById("resultsDisplay");
+    if(document.getElementById("careerChoice2").checked){
+        resultText.textContent = "Correct";
+        resultText.style="color:green";
+    }else{
+        resultText.textContent = "Incorrect";
+        resultText.style="color:red";
+    }
 }
